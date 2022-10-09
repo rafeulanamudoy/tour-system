e8d546febe1fc265778339a4928a32d76e674119
rafeulanamudoy <rafeuludoy@gmail.com>
2022-10-09 17:05:20 +0600

    app.js file changed

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const cors = require("cors");
 const app = express()
 app.use(express.json());
 app.use(cors());
-
+const mongoose = require("mongoose");
+const colors = require("colors");
+require('dotenv').config();
+const source = process.env.DATABASE_LOCAL;
 
 const tourRoute = require("./routes/tourPackage.routes");
 const tourRoute2 = require("./routes/tourPackage2.route")
@@ -11,6 +14,19 @@ const tourRoute2 = require("./routes/tourPackage2.route")
 app.get("/", async (req, res, next) => {
 
     res.send(" api is working  properly ")
+})
+mongoose.connect(source, { useNewUrlParser: true })
+    .then(() => {
+
+        console.log("database connect succesfully".green)
+        console.log(source)
+    })
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`app is listening on port ${port}`)
+
+
 })
 
 app.use("/api/v1/tours", tourRoute);
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
-const app = require("./app");
+// const mongoose = require("mongoose");
+// const app = require("./app");
 
-const colors = require("colors");
-require('dotenv').config();
-const source = process.env.DATABASE_LOCAL;
-mongoose.connect(source, { useNewUrlParser: true })
-    .then(() => {
+// const colors = require("colors");
+// require('dotenv').config();
+// const source = process.env.DATABASE_LOCAL;
+// mongoose.connect(source, { useNewUrlParser: true })
+//     .then(() => {
 
-        console.log("database connect succesfully".green)
-        console.log(source)
-    })
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`app is listening on port ${port}`)
+//         console.log("database connect succesfully".green)
+//         console.log(source)
+//     })
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//     console.log(`app is listening on port ${port}`)
 
 
-})
+// })
